134a17981691eb6f49a0e2ded7fa5d5b7986b35d
CtrlACtrlCtrlV <140812992+CtrlACtrlCtrlV@users.noreply.github.com>
2026-03-02 14:01:26 +0300

    5-th ready

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 print("Hello, ZA-WARKA-CHAY-ZAVARY!")
-=======
 import random 
 import turtle
 from tkinter import Tk
 #this has been tracking
 
 print("Hello, world!")
->>>>>>> origin/aboba
